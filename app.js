9784e0b2752d133e762df512913c38f47a840f0a
Matt Williams <matwilliams@hotmail.com>
2019-02-23 17:23:12 +0000

    Correlating items to attachables

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const express = require('express');
 const QuickBooks = require('node-quickbooks')
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
 
 const PORT = process.env.PORT || 8080;
 const QB_CLIENT_ID = process.env.QB_CLIENT_ID;
@@ -17,16 +18,48 @@ const qbo = new QuickBooks(QB_CLIENT_ID,
                            true,
                            true,
                            34,
-                           "2.0");
+                           '2.0');
  
 const app = express();
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
-  qbo.findCustomers({
-    fetchAll: true
-  }, function(e, customers) {
-    res.send(`Hello ${customers.QueryResponse.Customer[0].DisplayName}`);
-  })
+  qbo.findAttachables((e, attachables) => {
+    if (e) {
+      return res.status(500).json(e);
+    }
+
+    var attachableUrls = {};
+    attachables = attachables.QueryResponse.Attachable;
+    for (var ii = 0; ii < attachables.length; ii++) {
+      var attachable = attachables[ii];
+      if (attachable.AttachableRef) {
+        for (var jj = 0; jj < attachable.AttachableRef.length; jj++) {
+          var attachableRef = attachable.AttachableRef[jj];
+          if (attachableRef.EntityRef.type == 'Item') {
+            attachableUrls[attachableRef.EntityRef.value] = attachable.TempDownloadUri;
+          }
+        }
+      }
+    }
+
+    qbo.findItems((e, items) => {
+      if (e) {
+        return res.status(500).json(e);
+      }
+
+      var itemUrls = [];
+      items = items.QueryResponse.Item;
+      for (var ii = 0; ii < items.length; ii++) {
+        var item = items[ii];
+        itemUrls.push({'Id': item.Id, 'Url': attachableUrls[item.Id]});
+      }
+
+      res.render('attachables', {'Items': itemUrls});
+    });
+  });
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
