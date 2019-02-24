271f8fe5dffcbe60dbddb33e52c2b8c0c14d000e
Matt Williams <matwilliams@hotmail.com>
2019-02-24 10:49:57 +0000

    Basic score ranking

--- app.js
+++ app.js
@@ -6,7 +6,20 @@ const RssParser = require('rss-parser');
 const parse5 = require('parse5');
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
+const fs = require('fs');
 
+const qbo = new QuickBooks(process.env.QUICKBOOKS_CLIENT_ID,
+                           process.env.QUICKBOOKS_CLIENT_SECRET,
+                           process.env.QUICKBOOKS_ACCESS_TOKEN,
+                           null,
+                           process.env.QUICKBOOKS_REALM_ID,
+                           true,
+                           false,
+                           35,
+                           '2.0');
+const clarifai = new Clarifai.App({
+  apiKey: process.env.CLARIFAI_API_KEY
+});
 const rssParser = new RssParser();
 
 async function getRssImages(feedUrl) {
@@ -46,21 +59,53 @@ async function getScores(feedUrl) {
   return scoreById;
 }
 
-const qbo = new QuickBooks(process.env.QUICKBOOKS_CLIENT_ID,
-                           process.env.QUICKBOOKS_CLIENT_SECRET,
-                           process.env.QUICKBOOKS_ACCESS_TOKEN,
-                           null,
-                           process.env.QUICKBOOKS_REALM_ID,
-                           true,
-                           false,
-                           35,
-                           '2.0');
+function getItems() {
+  return new Promise((resolve, reject) => {
+    qbo.findItems((err, items) => {
+      if (err) {
+        return reject(err);
+      } 
+      resolve(items.QueryResponse.Item);
+    });
+  });
+}
 
-const clarifai = new Clarifai.App({
-  apiKey: process.env.CLARIFAI_API_KEY
-});
+function getAttachables() {
+  return new Promise((resolve, reject) => {
+    qbo.findAttachables((err, attachables) => {
+      if (err) {
+        return reject(err);
+      } 
+      resolve(attachables.QueryResponse.Attachable);
+    });
+  });
+}
 
-//getScores('https://www.pinterest.co.uk/matwilliams2875/feed.rss').then(scores => console.log(scores));
+async function getItemsWithAttachables() {
+  var items_attachables = await Promise.all([getItems(), getAttachables()]);
+  var items = items_attachables[0];
+  var attachables = items_attachables[1];
+
+  var itemUrls = {};
+  attachables.forEach(attachable => {
+    if (attachable.AttachableRef) {
+      attachable.AttachableRef.forEach(attachableRef => {
+        if (attachableRef.EntityRef.type == 'Item') {
+          itemUrls[attachableRef.EntityRef.value] = attachable.TempDownloadUri;
+        }
+      });
+    }
+  });
+
+  items = items.filter(item => item.Type == 'Inventory');
+  items.forEach(item => {
+    item.Url = itemUrls[item.Id];
+  });
+
+  return items;
+}
+
+//getItemsWithAttachables().then(i => console.log(i));
 
 const app = express();
 app.use(express.static('static'))
@@ -68,57 +113,42 @@ app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.get('/', (req, res) => {
-  qbo.findAttachables((err, attachables) => {
-    if (err) {
-      return res.status(500).json(err);
+app.get('/', (req, res) => res.redirect('/report.html'))
+app.get('/report.html', async (req, res) => {
+  try {
+    items = await getItemsWithAttachables();
+    res.render('attachables', {'items': items});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+app.get('/report2.html', async (req, res) => {
+  try {
+    var items = await getItemsWithAttachables();
+    var oldInputs = await clarifai.inputs.list({perPage: 1000});
+
+    var oldInputsById = {};
+    for (var ii = 0; ii < oldInputs.length; ii++) {
+      var oldInput = oldInputs[ii];
+      oldInputsById[oldInput.id] = oldInput;
+    };
+  
+    var inputs = items.filter(item => !!item.Url && !oldInputsById[item.Id]).map(item => ({'id': item.Id, 'url': item.Url, 'metadata': {'quickbooks_realm_id': process.env.QUICKBOOKS_REALM_ID}}));
+    if (inputs.length > 0) {
+      console.log("Adding to Clarifai", inputs);
+      await clarifai.inputs.create(inputs);
     }
 
-    var itemUrls = {};
-    attachables.QueryResponse.Attachable.forEach(attachable => {
-      if (attachable.AttachableRef) {
-        attachable.AttachableRef.forEach(attachableRef => {
-          if (attachableRef.EntityRef.type == 'Item') {
-            itemUrls[attachableRef.EntityRef.value] = attachable.TempDownloadUri;
-          }
-        });
-      }
+    var scores = await getScores('https://www.pinterest.co.uk/matwilliams2875/feed.rss');
+    items.forEach(item => {
+      item.Score = (scores[item.Id] * 100).toFixed(1) + '%';
     });
 
-    qbo.findItems((err, items) => {
-      if (err) {
-        return res.status(500).json(err);
-      }
-
-      var items = items.QueryResponse.Item;
-      items = items.filter(item => item.Type == 'Inventory');
-      items.forEach(item => {
-        item.Url = itemUrls[item.Id];
-      });
-
-      clarifai.inputs.list({perPage: 1000}).then(oldInputs => {
-        var oldInputsById = {};
-        for (var ii = 0; ii < oldInputs.length; ii++) {
-          var oldInput = oldInputs[ii];
-          oldInputsById[oldInput.id] = oldInput;
-        };
-  
-        var inputs = items.filter(item => !!item.Url && !oldInputsById[item.Id]).map(item => ({'id': item.Id, 'url': item.Url, 'metadata': {'quickbooks_realm_id': process.env.QUICKBOOKS_REALM_ID}}));
-        if (inputs.length > 0) {
-          console.log("Adding to Clarifai", inputs);
-          clarifai.inputs.create(inputs).then(() => {
-            res.render('attachables', {'items': items});
-          }, err => {
-            res.status(500).json(err);
-          });
-        } else {
-          res.render('attachables', {'items': items});
-        }
-      }, err => {
-        res.status(500).json(err);
-      });
-    });
-  });
+    fs.writeFile('items.json', JSON.stringify(items, null, 2), (err) => {if (err) {console.log(err)}});
+    res.render('attachables', {'items': items});
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 const PORT = process.env.PORT || 8080;
